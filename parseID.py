7ce3f7822dd31cb2bdb7fd719a92d59684f64854
Taylor Peterson <tayloredwardpeterson@gmail.com>
2015-01-13 09:08:19 -0800

    Spreadsheet checking on separate thread; can now read card and access spreadsheet consistently.

--- parseID.py
+++ parseID.py
@@ -1,22 +1,18 @@
-# Key logging derived from
-# http://antihackingtutorials.blogspot.com/2012/06/in-this-tutorial-we-will-show-you-how.html
-
-import gspread
-import pyHook
+import gspread # Google Spreadsheets Python API
+import pyHook # Provides callbacks for keyboard events
 import pythoncom
-import re
+import re # Regular expressions
+
 import win32api
 import win32con
 import win32console
 import win32gui
 
+import threading
+
+googleAccount = gspread.login('hmc.machine.shop@gmail.com', 'orangecow')
+worksheet = googleAccount.open("Python Testing").sheet1
 idBuffer = []
-
-# Login with your Google account
-gc = gspread.login('hmc.machine.shop@gmail.com', 'orangecow')
-
-# Open a worksheet from spreadsheet with one shot
-wks = gc.open("Python Testing").sheet1
 
 def OnKeyboardEvent(event):
     addCharToBuffer(chr(event.Ascii))
@@ -26,25 +22,29 @@ def OnKeyboardEvent(event):
         idNumber = idCheck.group()[2:-2]
         print "Your ID number is: " + idNumber
 
-        #shopUser = wks.find(idNumber)
-        #print "Welcome, " + (wks.cell(shopUser.row, shopUser.col - 1).value)
+        t = threading.Thread(target = getShopUser, args = (idNumber, ))
+        t.start()        
         
-    return True 
+    return True
+
+def getShopUser(idNumber):
+    shopUser = worksheet.find(idNumber)
+    shopUserName = worksheet.cell(shopUser.row, shopUser.col - 1).value
+    print shopUserName
 
 def addCharToBuffer(char):
     if len(idBuffer) >= 12:
         del idBuffer[0]
     idBuffer.append(char)
 
+def main():
+    # Start up the key logger
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    hm.HookKeyboard()
+    pythoncom.PumpMessages()
 
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-hm.HookKeyboard()
-pythoncom.PumpMessages()
+if __name__ == "__main__":
+    main()
 
-
-
-
-
-
-
+# Todo: write unit tests
