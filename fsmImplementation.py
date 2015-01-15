2f7ee3cc70325062ca897df73476071880dda4c2
Taylor Peterson <tayloredwardpeterson@gmail.com>
2015-01-15 08:38:12 -0800

    Deleted commented-out code.

--- fsmImplementation.py
+++ fsmImplementation.py
@@ -28,30 +28,44 @@ class BoardFsm():
 
         self.state_data = {
             CLOSED : ("Shop closed.\nProctor swipe to open.",
-                      {event.CARD_SWIPE_KEY : self.closed_process_card_swipe}),
+                      {event.CARD_SWIPE : self.closed_process_card_swipe}),
             
             OPENING : ("Starting up!\n\rType cancel or flip_switch_off: ",
                        {"cancel" : self.standby_state,
-                        "flip_switch_off" : self.on_shop_open}),
+                        "flip_switch_off" : self.opening_process_switch_flip}),
 
             STANDBY : ("Shop open. Board locked\n\rPOD swipe to unlock.",
-                      {event.CARD_SWIPE_KEY : self.standby_process_card_swipe}),
+                      {event.CARD_SWIPE : self.standby_process_card_swipe}),
 
             UNLOCKED : ("Board Unlcked.\n\rAll inputs available.",
                         {"cancel" : self.standby_state,
-                         event.CARD_SWIPE_KEY : self.addUser,
-                         "card_remove" : self.removeUser,
+                         event.CARD_SWIPE : self.unlocked_process_card_swipe,
+                         "card_remove" : self.remove_user_state,
                          "clear_money" : self.clearMoney,
                          "change_pod" : self.changePod,
                          "flip_switch_on" : self.closeShop}),
 
-            ADDING_USER
+            ADDING_USER : ("Adding user.\n\rSwipe another card, insert into slot, or cancel.",
+                           {event.CARD_SWIPE : self.adding_user_process_card_swipe,
+                            "card_insert" : self.adding_user_s_process_slot,
+                            "cancel" : self.standby_state}),
 
-            REMOVING_USER
+            ADDING_USERS : ("Adding users.\n\rInsert both cards into slot or cancel.",
+                            {"card_insert" : self.adding_user_s_process_slot,
+                             "cancel" : self.standby_state}),
 
-            CLEARING_DEBT
+            REMOVING_USER : ("Removing user(s).\n\rreinsert or insert card; or press clear or charge.",
+                             {"card_insert" : removing_user_process_slot,
+                              "clear" : removing_user_process_clear,
+                              "charge" : removing_user_process_charge}),
 
-            CHANGING_POD
+            CLEARING_DEBT : ("Clearning user debt.\n\rSwipe user card or cancel.",
+                             {event.CARD_SWIPE : self.clearing_debt_process_card_swipe,
+                              "cancel" : self.standby_state}),
+
+            CHANGING_POD : ("Changing POD status.\n\rSwipe proctor card or cancel.",
+                            {event.CARD_SWIPE : self.changing_pod_process_card_swipe,
+                             "cancel" : self.standby_state}),
 
             CLOSING
 
@@ -59,7 +73,9 @@ class BoardFsm():
         self.unauthorized_messages = {
             "default" : DEFAULT_ERROR_MESSAGE,
             "invalid user" : "User does not have permissions for that action.",
-            "invalid card swipe" : "ERR - Ignoring swipe. Please confirm."}
+            "invalid card swipe" : "ERR - Ignoring swipe. Please confirm.",
+            "invalid card removal" : "ERR - Reinsert card(s), or confirm students have left." # send tsk email
+            }
 
     def run_fsm(self):
         cargo = None
@@ -79,31 +95,6 @@ class BoardFsm():
 
     # =====================================
 
-    def closed_process_card_swipe(self, user, ignore_me):
-        if user.proctor:
-            return (OPENING, user)
-        else:
-            return self.unauthorized("invalid user")
-
-    def on_shop_open(self, ignore_me, user):
-        self.shop.state = "open"
-        self.shop.pod_list.append(user)
-        print "    Shop now open with proctor now on duty."
-        # log event
-        winsound.Beep(500,250) # old-school mac startup sound
-        return self.standby_state()
-    
-
-    def standby_process_card_swipe(self, user):
-        if user.proctor and self.shop.state == "closed":
-            return self.openShop(user)
-        elif user in self.shop.pod_list:
-            return self.unlockBoard(input)
-        else:
-            return self.unauthorized("invalid user")
-
-    # ========================================   
-
     def unauthorized(self, error_message = "default"):
         winsound.PlaySound('SystemExclamation', winsound.SND_ALIAS)
 
@@ -112,135 +103,78 @@ class BoardFsm():
         userInput = raw_input("    press enter to return to standby.")
         return self.standby_state()
 
+    def closed_process_card_swipe(self, user, ignore_me):
+        if user.proctor:
+            return (OPENING, user)
+        else:
+            return self.unauthorized("invalid user")
 
-
-                
-    def addUser(input):
-        print "\nAdding user to shop"
-        while True:
-            userInput = raw_input("    error, cleared: ")
-
-            if userInput == "error": # user not shop certified or owes money
-                winsound.Beep(500, 250)
-                # display error)
-                return unauthorized(input)
-            elif userInput == "cleared":
-                return addUserOrAssignMachine(input)
-            else:
-                print "        invalid input, try again"
-
-    def addUserOrAssignMachine(input):
-        print "\nAdding another user or assigning user to machine"
-        while True:
-            userInput = raw_input("    cancel, slot, cardRemoved, unauthorizedSwipe, verifiedSwipe: ")
-            if userInput == "cancel":
-                return standby_state()
-            elif userInput == "slot":
-                print "    User added to spot"
-                return standby_state()
-            elif userInput == "cardRemoved":
-                # buzz
-                print "        display error - reinsert card"
-                input = raw_input("    confirm, reinsert: ")
-                if input == "confirm":
-                    print "        silly user - send tsk email"
-                elif input == "reinsert":
-                    print "        continuing..."
-            elif userInput == "unauthorizedSwipe":
-                #buzz
-                print "unauthorized user"
-                raw_input("    press enter to continue")
-            elif userInput == "verifiedSwipe":
-                print "         additional shop user added"
-                return assignTwoUsersToMachine(input)    
-            else:
-                print "        invalid input, try again"
-
-    def assignTwoUsersToMachine(input):
-        print "\nAssigning two users"
-        while True:
-            userInput = raw_input("    cancel, slot")
-            if userInput == "cancel":
-                return standby_state()
-            elif userInput == "slot":
-                print "        two users in spot"
-                # chime
-                return standby_state()
-            else:
-                print "        invalid input, try again"       
-
-    def removeUser(input):
-        print "\nRemoving user from shop"
-        while True:
-            userInput = raw_input("    reinsert, insert, clear, charge: ")
-            if userInput == "reinsert": ## Card reinserted in same slot
-                return standby_state() # abort removal
-            elif userInput == "insert": ## Card inserted in different slot 
-                print "        User changed machines" # Transfer user(s) to new machine
-                return standby_state()
-            elif userInput == "clear": # press clear
-                print "        User allowed to leave shop" # discharge user from system
-                # chime
-                return standby_state()
-            elif userInput == "charge": # press charge
-                print "        Adding money owed to user account"
-                # sad trombone
-                return standby_state()
-            else:
-                print "        invalid, try again"
-
-    def clearMoney(input):
-        print "\nClearing user debt"
-        while True:
-            userInput = raw_input("    cancel, swipe: ")
-            if userInput == "cancel":
-                return standby_state()
-            elif userInput == "swipe":
-                print "        User account zeroed"
-                # chaching sound effect
-                return standby_state()
-            else:
-                print "        invalid, try again"
-
-    def changePod(input):
-        print "\nModifying proctor on duty status"
-        while True:
-            userInput = raw_input("    cancel, offDutyProctor, onDutyProctor: ")
-            if userInput == "cancel":
-                return standby_state()
-            elif userInput == "offDutyProctor" and self.shop.state == "open":
-                print "        proctor is now on duty"
-                return standby_state()
-            elif userInput == "onDutyProctor": # and another proctor on duty or shop closed
-                print "        proctor is now off duty"
-                return standby_state()
-            elif userInput == "onDutyProctor": # and no other proctors and shop is open
-                #Denied, buzz, display error "Shop must have proctor", wait for confirmation, return to process
-                return standby_state()
-            else:
-                print "        invalid input, try again"
-
-    def closeShop(input):
-        print "\nClosing the shop"
-        userInput = raw_input("    clear, occupied: ")
-        if userInput == "clear":
-            print "        Entering sleep mode... goodbye"
-            return standby_state()
-        elif userInput == "occupied":
-            print "        You can't close the shop! Shop users still present!"
-            return standby_state()
-
-    # add clarification as to whether a state is automated or user-driven?
-        # state changes occur only after user input
+    def opening_process_switch_flip(self, ignore_me, user):
+        self.shop.open = True
+        self.shop.pod_list.append(user)
+        print "    Shop now open with proctor now on duty."
+        # log event
+        winsound.Beep(500,250) # old-school mac startup sound
+        return self.standby_state()
     
-    # Todo: write unit tests
-# Need to use daemon threads?
-    # need to joing non-keylogging threads?
-    # have proctor safety test update the proctorness column
 
-    # wrap state handlers back in while trues, then add a third data member to the tuples
-    # that details how to get back to the desired state should things be done out of order. 
+    def standby_process_card_swipe(self, user, ignore_me):
+        if user in self.shop.pod_list:
+            return self.unlockBoard(input)
+        else:
+            return self.unauthorized("invalid user")
 
+    def unlocked_process_card_swipe(self, user, ignore_me):
+        if user.name == "Unauthorized":
+            return self.unauthorized("invalid user")
+        else:
+            return (ADD_USER_OR_ASSIGN_SLOT, user)
+
+    def unlocked_process_closing_shop(self, ignore_me, ignore_me):
+        # close shop if clear
+        # error otherwise
+
+    def adding_user_process_card_swipe(self, second_user, first_user):
+        if user.name == "Unauthorized":
+            return self.unauthorized("invalid user")
+        else:
+            return (ASSIGN_SLOT, (first_user, second_user))
+
+    def adding_user_s_process_slot(self, slot, users):
+        pass
+        # add users to correct slot
+
+    def removing_user_process_slot(self, slot, prev_slot):
+        pass
+        # return to standby or move user(s) to correct slot
+        
+    def removing_user_process_clear(self, slot, ignore_me):
+        pass
+        # discharge user from system
+
+    def removing_user_process_charge(self, slot, ignore_me):
+        pass
+        # add money owed to user account
+        # sad trombone
+
+    def clearing_debt_process_card_swipe(self, user, ignore_me):
+        pass
+        #chaching
+        #clear user account
+
+    def changing_pod_process_card_swipe(self, user, ignore_me):
+        if user.proctor and not user in shop.pod_list:
+            shop.pod_list.append(user)
+            return self.standby_state
+        elif user in shop.pod_list and len(shop.pod_list) > 1:
+            shop.pod_list.remove(user)
+            return self.standby_state
+        elif user.proctor:
+            return unauthorized("shop must have proctors")
+        else:
+            return unauthorized("invalid user")
+
+    def closing_shop
 
 def main():
     # Create a single input and a single output queue for all threads.
@@ -258,53 +192,4 @@ def main():
         #board.standby_state()
 
 if __name__== "__main__":
-    main()
-
-
-                              
-                            
-
-##        self.shop_closed_actions = {
-##            "card_swipe" : self.shop_closed_process_card_swipe}
-##
-##        self.standby_actions = {
-##            "card_swipe" : self.standby_process_card_swipe}
-        
-##        self.open_shop_actions = {
-##            "cancel" : self.standby_state,
-##            "flip_switch_off" : self.on_shop_open}
-
-##        self.unlock_board_actions = {
-##            "cancel" : self.standby_state,
-##            "card_swipe" : self.addUser,
-##            "card_remove" : self.removeUser,
-##            "clear_money" : self.clearMoney,
-##            "change_pod" : self.changePod,
-##            "flip_switch_on" : self.closeShop}
-                        
-
-    # =====================================
-
-##    def openShop(self, input):
-##        userInput = raw_input("\nStarting up! Type cancel or flip_switch_off: ")
-##        return self.open_shop_actions.get(userInput, self.unauthorized)()
-                
-
-
-    # ========================================
-
-##    def standby_state(self):
-##        print "\nStanding by. The shop is currently."
-##
-##        event = self.event_q.get()
-##
-##        try:
-##            self.standby_actions[event.key](event.data)
-##        except KeyError:
-##            self.unauthorized(event.error_msg)
-
-##    def unlockBoard(self, input):
-##        print "\nUnlocking  Board"
-##        userInput = raw_input(
-##            "You can: cancel, card_swipe, card_remove, clear_money, change_pod, or flip_switch_on")
-##        return self.unlock_board_actions.get(userInput, self.unauthorized)()
+    main()                       
