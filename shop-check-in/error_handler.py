38c2b6ee2c5e5f1f89462a08e74f81d0e46908ef
Alex Ozdemir <aozdemir@hmc.edu>
2015-02-15 20:04:45 -0800

    Built the usage logger. No tests yet.

--- shop-check-in/error_handler.py
+++ shop-check-in/error_handler.py
@@ -3,11 +3,10 @@ import fsm
 import shop_user
 import shop_check_in_exceptions
 import event
-import io_moderator
 from mailer import Mailer
 import winsound
 
-DEFAULT_ERROR_MESSAGE = "\0ACTION NOT\n\rRECOGNIZED. CNFM"
+DEFAULT_ERROR_MESSAGE = "\0Action not recognized."
 ERROR_RESOLVED = "error_resolved"
 ERROR_NOT_RESOLVED = "error_not_resolved"
 
@@ -18,35 +17,31 @@ class ErrorHandler(object):
         self._message_q = message_q
         self._shop = shop_
         self._mailer = Mailer()
+
         # TODO: Right now errors are sometimes passed in as events, sometimes errors. Homogenize.
         self._messages_to_display = {
-            shop_check_in_exceptions.NonexistentUserError: "\0NONEXISTENT\n\rUSER",
-            shop_check_in_exceptions.InvalidUserError: "\0ERR - INVALID\n\rUSER - CONFIRM",
-            shop_check_in_exceptions.MoneyOwedError: "\0ERR - USER OWES\n\rMONEY - CONFIRM",
-            shop_check_in_exceptions.NonProctorError: "\0ERR - USER IS\n\rNOT A PROCTOR",
-            shop_check_in_exceptions.NonPodError: "\0ERR - USER IS\n\rNOT A POD",
-            shop_check_in_exceptions.ShopOccupiedError: "\0ERR\n\rSHOP IS OCCUPIED",
-            shop_check_in_exceptions.OutOfDateTestError: "\0ERR - EXPIRED\n\rSAFETY TEST",
-            shop_check_in_exceptions.ShopAlreadyOpenError: "\0ERR SHOP\n\rALREADY OPEN",
-            shop_check_in_exceptions.PodRequiredError: "\0ERR - ONLY POD\n\rCANNOT SIGN OUT",
-            shop_user.DEFAULT_NAME: "\0ERR - LACK\n\r PERMISSIONS",
-            event.CARD_SWIPE:  "\0ERR - IGNORING\n\r SWIPE, CONFIRM",
-            event.CARD_REMOVE: "\0ERR -REINSRT OR\n\rCNFRM, SLOT: ",
-            event.CARD_INSERT: "\0ERR - UNINSERT\n\rSLOT:",
-            event.SWITCH_FLIP_OFF: "\0ERR - TURN\n\rSHOP BACK ON"
+            shop_check_in_exceptions.MoneyOwedError: "\0User owes money.",
+            shop_check_in_exceptions.NonProctorError: "\0User is not a proctor.",
+            shop_check_in_exceptions.ShopOccupiedError: "\0Shop is currently occupied.",
+            shop_check_in_exceptions.OutOfDateTestError: "\0Out-of-date safety test.",
+            shop_check_in_exceptions.ShopAlreadyOpenError: "\0Shop is already open.",
+            shop_check_in_exceptions.PodRequiredError: "\0Only Proctor can't sign out",
+            shop_user.DEFAULT_NAME: "\0User does not have permissions for that action.",
+            event.CARD_SWIPE: "\0ERR - Ignoring swipe. Please confirm.",
+            event.CARD_REMOVE: "\0ERR - Reinsert card(s) or confirm. Slot: ",
+            event.CARD_INSERT: "\0ERR - Remove card(s)",
             }
 
         self._no_confirm_state_error_combos = [
             (fsm.CLOSED, shop_check_in_exceptions.ShopUserError),
             (fsm.CLOSED, shop_check_in_exceptions.NonexistentUserError),
-            (fsm.CLOSED, shop_check_in_exceptions.NonProctorError),
             (fsm.STANDBY, shop_check_in_exceptions.UnauthorizedUserError)
         ]
 
         self._default_event_to_action_map = {
             event.CARD_INSERT: self._handle_card_insert_default,
             event.CARD_REMOVE: self._handle_card_remove_default,
-            event.BUTTON_CONFIRM: self._handle_confirm_default,
+            event.BUTTON_CONFIRM: self._handle_confirm_default
         }
 
         self._error_specific_event_to_action_map = {
@@ -57,14 +52,7 @@ class ErrorHandler(object):
             event.CARD_INSERT: {
                 event.CARD_REMOVE: self._handle_card_uninsert,
                 event.BUTTON_CONFIRM: self._handle_unrecognized_event
-            },
-            shop_check_in_exceptions.ShopOccupiedError: {
-                event.SWITCH_FLIP_ON: self._handle_shop_open
-            },
-            event.SWITCH_FLIP_OFF: {
-                event.SWITCH_FLIP_ON: self._handle_shop_open
             }
-
         }
 
     def _requires_no_confirmation(self, state, error):
@@ -78,7 +66,6 @@ class ErrorHandler(object):
         """
         Determines if e is an instance of error class template_e OR if e equals template_e
         """
-
         try:
             return isinstance(e, template_e)
         except TypeError:
@@ -87,16 +74,18 @@ class ErrorHandler(object):
     def handle_error(self, return_state, error, error_data=None):
         # winsound.PlaySound('SystemExclamation', winsound.SND_ALIAS)
 
+        self._store_error_frame(return_state, error, error_data)
+
         while True:
 
-            self._report_error(error, error_data)
+            self._report_error()
 
             if self._requires_no_confirmation(return_state, error):
                 return return_state
 
             next_event = self._event_q.get()
             if next_event.key == event.TERMINATE_PROGRAM:
-                # TODO: Find better way to terminate prgm from error, currently need 2 signals
+                # TODO: Find better way to termiinate prgm from error, currently need 2 signals
                 self._event_q.put(event.Event(event.TERMINATE_PROGRAM))
                 return return_state
 
@@ -107,15 +96,15 @@ class ErrorHandler(object):
             if result == ERROR_RESOLVED:
                 return return_state
 
-    def _report_error(self, error, error_data):
-        print 'going to report', error
-        error_msg = self._messages_to_display.get(error, DEFAULT_ERROR_MESSAGE)
-        error_msg += str(error_data) if error_data else ""
-        print 'with msg', error_msg
-        self._send_message_format_safe(error_msg)
+    def _store_error_frame(self, return_state, error, error_data):
+        self._return_state = return_state
+        self._error = error
+        self._error_data = error_data
 
-    def _send_message_format_safe(self, msg):
-        self._message_q.put(io_moderator.safe_format_msg(msg))
+    def _report_error(self):
+        error_msg = self._messages_to_display.get(self._error, DEFAULT_ERROR_MESSAGE)
+        error_msg += str(self._error_data) if self._error_data else ""
+        self._message_q.put(error_msg)
 
     def _get_action(self, error, next_event):
         try:
@@ -144,12 +133,11 @@ class ErrorHandler(object):
 
     def _handle_card_removed_not_reinserted(self, unused_data=None, old_slot=None):
         assert old_slot is not None
-        name = "".join(self._shop.get_user_s_name(old_slot))[:14]
-        self._send_message_format_safe("\0%s\n\rleft the shop!" % name)
+        self._message_q.put("User %s, left the shop!" %
+                            (str(self._shop.get_user_s_name_and_email(old_slot))))
         users = self._shop.get_user_s(old_slot)
         self._mailer._send_id_card_email_s(users)
         self._shop.discharge_user_s(old_slot)
-        return ERROR_RESOLVED
 
     def _handle_card_insert_default(self, new_slot, unused_error_data=None):
         self.handle_error(None, event.CARD_INSERT, new_slot)
@@ -164,7 +152,3 @@ class ErrorHandler(object):
 
     def _handle_unrecognized_event(self, unused_data=None, unused_error_data=None):
         return ERROR_NOT_RESOLVED
-
-    def _handle_shop_open(self, unused_date=None, unused_error_data=None):
-        return ERROR_RESOLVED
-
--- shop-check-in/shop.py
+++ shop-check-in/shop.py
@@ -1,19 +1,59 @@
 import shop_check_in_exceptions
+from datetime import datetime
 import shop_user
+from usage_logger import log_event
 
+NO_TIME = None
 SLOTS = xrange(30)
-
+LOG_HEADER = 'In Time,Out Time,Machine,Name,Email'
+POD_OPEN_MESSAGE = 'POD Opened the Shop'
+POD_CLOSE_MESSAGE = 'POD Closed the Shop'
+POD_ARRIVE_MESSAGE = 'POD Arrived at the Shop'
+POD_EXIT_MESSAGE = 'POD Left the Shop'
+SLOT_TO_MACHINE_MAP = { 0 : 'Slot 0',
+    1 : 'Slot 1',
+    2 : 'Slot 2',
+    3 : 'Slot 3',
+    4 : 'Slot 4',
+    5 : 'Slot 5',
+    6 : 'Slot 6',
+    7 : 'Slot 7',
+    8 : 'Slot 8',
+    9 : 'Slot 9',
+    10 : 'Slot 10',
+    11 : 'Slot 11',
+    12 : 'Slot 12',
+    13 : 'Slot 13',
+    14 : 'Slot 14',
+    15 : 'Slot 15',
+    16 : 'Slot 16',
+    17 : 'Slot 17',
+    18 : 'Slot 18',
+    19 : 'Slot 19',
+    20 : 'Slot 20',
+    21 : 'Slot 21',
+    22 : 'Slot 22',
+    23 : 'Slot 23',
+    24 : 'Slot 24',
+    25 : 'Slot 25',
+    26 : 'Slot 26',
+    27 : 'Slot 27',
+    28 : 'Slot 28',
+    29 : 'Slot 29'
+}
 
 class Shop(object):
     def __init__(self):
         self._open = False
         self._pods = []
         self._occupants = [[] for slot in SLOTS]
+        self._start_times = [NO_TIME for slot in SLOTS]
 
     def open_(self, user):
         if user.is_proctor() and not self._open:
             self._open = True
             self._pods.append(user)
+            self.log_pod_event(user, POD_OPEN_MESSAGE)
         elif user.is_proctor():
             raise shop_check_in_exceptions.ShopAlreadyOpenError
         else:
@@ -23,6 +63,7 @@ class Shop(object):
         if self.is_pod(user) and self._empty():
             self._pods = []
             self._open = False
+            self.log_pod_event(user, POD_CLOSE_MESSAGE)
         elif not self.is_pod(user):
             raise shop_check_in_exceptions.UnauthorizedUserError
         else:
@@ -35,21 +76,27 @@ class Shop(object):
         slot = int(slot)
         if all(user.is_shop_certified() for user in user_s):
             self._occupants[slot] = user_s
+            self._start_times[slot] = datetime.now()
 
     def replace_or_transfer_user(self, slot, prev_slot):
         slot = int(slot)
         prev_slot = int(prev_slot)
 
         if slot != prev_slot:
+            self.log_exit(slot)
             self._occupants[slot] = self._occupants[prev_slot]
+            self._start_times[slot] = NO_TIME
             self._occupants[prev_slot] = []
+            self._start_times[slot] = datetime.now()
         else:
             pass  # The user(s) remain in their current location.
 
     def discharge_user_s(self, slot):
         slot = int(slot)
         occupants = self._occupants[slot]
+        self.log_exit(slot)
         self._occupants[slot] = []
+        self._start_times[slot] = NO_TIME
         return occupants
 
     def get_user_s(self, slot):
@@ -63,10 +110,32 @@ class Shop(object):
     def change_pod(self, user):
         if user.is_proctor() and not self.is_pod(user):
             self._pods.append(user)
+            self.log_pod_event(user, POD_ARRIVE_MESSAGE)
         elif self.is_pod(user) and len(self._pods) > 1:
             self._pods.remove(user)
+            self.log_pod_event(user, POD_EXIT_MESSAGE)
         elif self.is_pod(user):
             raise shop_check_in_exceptions.PodRequiredError
 
     def _empty(self):
         return all(self._occupants[slot] == [] for slot in SLOTS)
+
+    def log_exit(self, slot):
+        occupants = self._occupants[slot]
+        for occupant in occupants:
+            start_time = self._start_times[slot]
+            end_time = datetime.now()
+            machine = SLOT_TO_MACHINE_MAP[slot]
+            name = occupant.name
+            email = occupant.email
+            log_mesage = '%s,%s,%s,%s,%s' % (start_time, end_time, machine, name, email)
+            log_event(log_mesage, LOG_HEADER)
+
+    def log_pod_event(self, user, msg):
+        start_time = end_time = datetime.now()
+        machine = msg
+        name = user.name
+        email = user.email
+        log_mesage = '%s,%s,%s,%s,%s' % (start_time, end_time, machine, name, email)
+        log_event(log_mesage, LOG_HEADER)
+
